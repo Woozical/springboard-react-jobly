5391f0b74fd699a6e6a1e68a62ffbba273ab5407
Mark <mark@example.dev>
2021-11-30 09:42:35 -0800

    barebones company details page

--- src/components/CompanyCard/index.js
+++ src/components/CompanyCard/index.js
@@ -1,18 +1,21 @@
 import { Card, CardBody, CardTitle, CardText } from "reactstrap";
+import { NavLink } from "react-router-dom";
 import "./style.css";
 
 const CompanyCard = ({handle, name, description, logo}) => {
   return (
     <Card className="CompanyCard">
-      <CardBody>
-        <CardTitle tag="h6">
-          {name}
-          {logo && <img alt={name} src={logo} className="float-right ml-5" />}
-        </CardTitle>
-        <CardText>
-          {description}
-        </CardText>
-      </CardBody>
+      <NavLink to={`/companies/${handle}`}>
+        <CardBody>
+          <CardTitle tag="h6">
+            {name}
+            {logo && <img alt={name} src={logo} className="float-right ml-5" />}
+          </CardTitle>
+          <CardText>
+            {description}
+          </CardText>
+        </CardBody>
+      </NavLink>
     </Card>
   )
 }
--- src/pages/CompanyDetail/index.js
+++ src/pages/CompanyDetail/index.js
@@ -1,7 +1,32 @@
-import Placeholder from "../../components/placeholder";
+// import Placeholder from "../../components/placeholder";
+import { useState, useEffect } from "react";
+import { useParams, Navigate } from "react-router";
+import JobList from "../../components/JobList";
+import JoblyAPI from "../../api";
 
 const CompanyDetailPage = () => {
-  return <Placeholder text="Company Detail" />;
+  const [company, setCompany] = useState({name: "", numEmployees: 0, description: "", jobs: []});
+  const { handle } = useParams();
+
+  /** Load in company data on initial page render */
+  useEffect( () => {
+    async function loadCompany(){
+      const c = await JoblyAPI.getCompany(handle);
+      // if !c, return 404 component
+      setCompany(c);
+    }
+    loadCompany();
+  },[])
+
+  // To Do: Make Company Info header component
+  return(
+    <main>
+      <h1>{company.name}</h1>
+      <h6>Employees: {company.numEmployees}</h6>
+      <p>{company.description}</p>
+      <JobList jobs={company.jobs} />
+    </main>
+  )
 }
 
 export default CompanyDetailPage;
