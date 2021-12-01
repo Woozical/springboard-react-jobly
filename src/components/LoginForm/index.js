12fd04fd023ad769f01b1c6d2bef528177c4aed4
Mark <mark@example.dev>
2021-12-01 12:29:41 -0800

    begun work on form validation and API error handling

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,32 +1,64 @@
 import { useState } from "react";
-import { Form, Card, CardBody, Label, FormGroup, Button, Input } from "reactstrap";
+import { Form, Card, CardBody, Label, FormGroup, Button, Input, FormFeedback } from "reactstrap";
+import { validateFormData } from "../../utils";
+import schema from "../../schemas/LoginForm.json";
 
 const DEFAULT_STATE = {username: "", password: ""};
+const DEFAULT_FEEDBACK_STATE = {username: [], password: [], failedAuth: false};
 
 const LoginForm = ({submitCallback}) => {
   const [formData, setFormData] = useState(DEFAULT_STATE);
+  const [formFeedback, setFormFeedback] = useState(DEFAULT_FEEDBACK_STATE);
   const handleChange = (evt) => {
     const {name, value} = evt.target;
     setFormData( data => ({...data, [name] : value}) );
   }
   const handleSubmit = async (evt) => {
     evt.preventDefault();
-    await submitCallback(formData);
-    // setFormData(DEFAULT_STATE);
+    const res = validateFormData(formData, schema);
+    if (res.success){
+      const loginAttempt = await submitCallback(formData);
+      console.log(loginAttempt);
+      if (!loginAttempt){
+        setFormFeedback(f => ({...DEFAULT_FEEDBACK_STATE, failedAuth : true}));
+        setFormData(DEFAULT_STATE);
+      }
+    } else {
+      setFormFeedback({...DEFAULT_FEEDBACK_STATE, ...res.errors});
+    }
   }
   return (
     <Card>
       <CardBody>
         <Form onSubmit={handleSubmit}>
           <FormGroup>
-          <Label htmlFor="login-username"><b>Username</b></Label>
-          <Input onChange={handleChange} type="text" id="login-username" name="username" value={formData.username} />
+            <Label htmlFor="login-username"><b>Username</b></Label>
+            <Input
+              onChange={handleChange}
+              type="text"
+              id="login-username"
+              name="username"
+              value={formData.username} 
+              invalid={formFeedback.username.length > 0} />
+              <FormFeedback>
+                {formFeedback.username.map(msg => <p>{msg}</p>)}
+              </FormFeedback>
           </FormGroup>
 
           <FormGroup>
           <Label htmlFor="login-password"><b>Password</b></Label>
-          <Input onChange={handleChange} type="password" id="login-password" name="password" value={formData.password} />
+          <Input
+            onChange={handleChange}
+            type="password"
+            id="login-password"
+            name="password"
+            value={formData.password}
+            invalid={formFeedback.password.length > 0} />
+            <FormFeedback>
+              {formFeedback.password.map(msg => <p>{msg}</p>)}
+            </FormFeedback>
           </FormGroup>
+          {formFeedback.failedAuth && <p className="text-danger text-center">Incorrect username or password.</p> }
           <div className="text-center">
             <Button color="primary">Submit</Button>
           </div>
--- src/utils.js
+++ src/utils.js
@@ -11,4 +11,43 @@ function addCommas(num) {
   return strDec ? subStrings.reverse().join(',') + '.' + strDec : subStrings.reverse().join(',');
 }
 
-export { addCommas }
+function validateFormData(formData, schema){
+  const validateRequired = (input) => (input !== null && input !== undefined && input !== "");
+  const validateMinLength = (input, min) => (input.length >= min);
+  const validateMaxLength = (input, max) => (input.length <= max);
+  const validateType = (input, type) => (typeof input === type);
+  const errors = {};
+  for (let field of Object.keys(schema)){
+    errors[field] = [];
+    for (let validator of Object.keys(schema[field])){
+      switch (validator){
+        case "required":
+          if (!validateRequired(formData[field])) errors[field].push("This field is required");
+          break;
+        case "minLength":
+          if (!validateMinLength(formData[field], schema[field][validator])){
+            errors[field].push(`This field's minimum length is ${schema[field][validator]}.`);
+          }
+          break;
+        case "maxLength":
+          if (!validateMaxLength(formData[field], schema[field][validator])){
+            errors[field].push(`This field's maximum length is ${schema[field][validator]}.`);
+          }
+          break;
+        case "type":
+          if (!validateType(formData[field], schema[field][validator])){
+            errors[field].push(`This field's must be of type ${schema[field][validator]}.`);
+          }
+          break;
+        default:
+          throw new Error(`Error: attempted to use unsupported validator: ${validator}`);
+      }
+    }
+  }
+  for (let field of Object.keys(errors)){
+    if (errors[field].length < 1) delete errors[field];
+  }
+  return Object.keys(errors).length > 0 ? {errors, success : false } : { success: true };
+}
+
+export { addCommas, validateFormData }
