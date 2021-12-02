d4e25d2106b850b691a32362fa44f726d13f18a2
Mark <mark@example.dev>
2021-12-01 16:49:56 -0800

    logo fix

--- src/components/CompanyHeader/index.js
+++ src/components/CompanyHeader/index.js
@@ -1,11 +1,11 @@
-const CompanyHeader = ({name, numEmployees, description, jobs, logo}) => {
+const CompanyHeader = ({name, numEmployees, description, jobs, logoUrl}) => {
   return (
     <div className="CompanyHeader text-start">
       <div className="row">
         <h1 className="col-10">
           {name}
         </h1>
-        {logo && <img className="col-2" src={logo} alt={name} />}
+        {logoUrl && <img className="col-2" src={logoUrl} alt={name} />}
       </div>
       <div className="text-start">
         <h6 className="text-secondary">
